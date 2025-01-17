310129ecddec635e4b92c3fff28bafbf233df23e
Gio Li <1193078960@qq.com>
2025-01-17 15:07:14 +0800

    chore: update reply

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -3,5 +3,7 @@ import { Bot } from "grammy";
 export const bot = new Bot("7671252515:AAHe559e5GZuRTldOmHiE99QBrpxCtuXr0k");
 
 bot.on("message", async (ctx) => {
-  await ctx.reply("Hi there!");
+  await ctx.replyWithAudio("https://ai-pet-two.vercel.app/dog.ogg", {
+    caption: "???",
+  });
 });
