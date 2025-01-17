afa461e8e975745397d4814759178bcabfe11c91
Gio Li <1193078960@qq.com>
2025-01-17 14:49:18 +0800

    init

---
+++ src/app/bot/route.ts
@@ -0,0 +1,9 @@
+import { bot } from "@/lib/bot";
+import { webhookCallback } from "grammy";
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export const config = { runtime: "edge" };
+
+export const POST = webhookCallback(bot, "std/http");
--- src/pages/api/bot.ts
+++
@@ -1,6 +0,0 @@
-import { bot } from "@/lib/bot";
-import { webhookCallback } from "grammy";
-
-export const config = { runtime: "edge" };
-
-export default webhookCallback(bot, "std/http");
