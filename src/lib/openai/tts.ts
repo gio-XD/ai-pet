f8d5a10bcaeaeb0fe6ef1224fa6a43653163e641
Gio Li <1193078960@qq.com>
2025-01-17 20:58:29 +0800

    update

--- src/lib/openai/tts.ts
+++ src/lib/openai/tts.ts
@@ -1,9 +1,25 @@
 import { openai } from "./index";
 
+const genText = async () => {
+  const aiout = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "user",
+        content: `你是一个尖酸刻薄的批评家，我最近炒币亏了很多钱，你用中文狠狠骂我一顿,言简意赅`,
+      },
+    ],
+    stream: false,
+  });
+
+  return aiout?.choices?.[0]?.message?.content;
+};
 export const openAiTts = async () => {
+  const text = await genText();
+
+  if (!text) throw new Error("no text!");
   const mp3 = await openai.audio.speech.create({
-    input:
-      "Ensure that the Blob contains valid binary data appropriate for the file type you're sending (e.g., audio data for audio files).",
+    input: text,
     model: "tts-1",
     voice: "alloy",
   });
