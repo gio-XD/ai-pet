6cbacf6cd96eb9373983372e98c5d73cef844f8c
Gio Li <1193078960@qq.com>
2025-01-17 15:50:29 +0800

    update

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -3,11 +3,7 @@ import { Bot, InputFile } from "grammy";
 export const bot = new Bot("7671252515:AAHe559e5GZuRTldOmHiE99QBrpxCtuXr0k");
 
 bot.on("message", async (ctx) => {
-  const filearrayBuffer = await fetch("https://ai-pet-two.vercel.app/dog.ogg")
-    .then((res) => res.blob())
-    .then((blob) => blob.arrayBuffer());
-
-  const file = new InputFile(new Uint8Array(filearrayBuffer));
+  const file = new InputFile("../../public/dog.ogg");
 
   await ctx.replyWithVoice(file, {
     caption: "???",
