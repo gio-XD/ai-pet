4bcfd8e84be9bfd91d7a4d5ff5eea3e71373214c
Gio Li <1193078960@qq.com>
2025-01-18 11:28:42 +0800

    update

--- src/app/globalVar.ts
+++ src/app/globalVar.ts
@@ -1,9 +1,11 @@
 export type TempStorage = {
   mbti: string;
-  phone: string;
+  pet: string; // 精神动物
+  phone: string; // +区号手机号;
   preferences: "low" | "medium" | "high"; //投资风险偏好;
-  address: string; // ,
-  personality: "gentle" | "rough";
+  address: string; // solana地址,
+  monitors: string[]; // 我要监控的solana地址
+  personality: "gentle" | "rough"; // gentle:温柔 rough:毒舌;
 };
 
 // eslint-disable-next-line prefer-const
@@ -13,6 +15,8 @@ export let tempStorage: TempStorage = {
   preferences: "low", //投资风险偏好;
   address: "",
   personality: "gentle",
+  pet: "",
+  monitors: [],
 };
 
 export const setStorage = (newValue: TempStorage) => {
